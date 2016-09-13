5b39ba50c2681337dedb8d3ef667d9836416be12
Marshall Thompson <marshall@creativeideal.net>
2016-09-12 22:07:58 -0600

    Add test for auth.

--- test/index.js
+++ test/index.js
@@ -17,3 +17,18 @@ describe('donejs-vscode', function() {
     assert.fileContent('.vscode/launch.json', /[ "--develop", "--port", "8080"]/);
   });
 });
+
+describe('donejs-vscode with auth', function() {
+  before(function(done) {
+    helpers.run(path.join(__dirname, '../default'))
+      .inTmpDir(function(dir){
+        fs.copySync(path.join(__dirname, 'test-auth-package.json'), path.join(dir, 'package.json'));
+      })
+      .on('end', done);
+  });
+
+  it('should write .vscode/launch.json file', function() {
+    assert.file(['.vscode/launch.json']);
+    assert.fileContent('.vscode/launch.json', /[ "--develop", "--port", "8080", "--auth\-cookie", "feathers\-jwt", "auth\-domains", "canjs.com,donejs.com"]/);
+  });
+});
